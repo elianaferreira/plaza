f0282e87f798b3e1e3f20c34eaa05685953dcb37
Eliana Ferreira <ebferreiran@gmail.com>
2022-07-26 17:34:29 -0400

    minor fixes

--- script.js
+++ script.js
@@ -19,34 +19,36 @@ dragElement(document.getElementById('plaza17'));
 dragElement(document.getElementById('plaza18'));
 
 
-function dragElement(terrariumElement) {
+function dragElement(elementoDePlaza) {
+	//establecer 4 posiciones para posicionar en la pantalla
 	let pos1 = 0,
 		pos2 = 0,
 		pos3 = 0,
 		pos4 = 0;
-	terrariumElement.onpointerdown = pointerDrag;
+	elementoDePlaza.onpointerdown = arrastrarPuntero;
 
-	function pointerDrag(e) {
-		e.preventDefault();
-		console.log(e);
-		pos3 = e.clientX;
-		pos4 = e.clientY;
-		document.onpointermove = elementDrag;
-		document.onpointerup = stopElementDrag;
-	}
 
-	function elementDrag(e) {
-		pos1 = pos3 - e.clientX;
-		pos2 = pos4 - e.clientY;
-		pos3 = e.clientX;
-		pos4 = e.clientY;
-		console.log(pos1, pos2, pos3, pos4);
-		terrariumElement.style.top = terrariumElement.offsetTop - pos2 + 'px';
-		terrariumElement.style.left = terrariumElement.offsetLeft - pos1 + 'px';
-	}
+function arrastrarPuntero(e) {
+	e.preventDefault();
+	console.log(e);
+	pos3 = e.clientX;
+	pos4 = e.clientY;
+	document.onpointermove = arrastrarElemento;
+	document.onpointerup = detenerArrastreElemento;
+}
 
-	function stopElementDrag() {
-		document.onpointerup = null;
-		document.onpointermove = null;
-	}
+function arrastrarElemento(e) {
+	pos1 = pos3 - e.clientX;
+	pos2 = pos4 - e.clientY;
+	pos3 = e.clientX;
+	pos4 = e.clientY;
+	console.log(pos1, pos2, pos3, pos4);
+	elementoDePlaza.style.top = elementoDePlaza.offsetTop - pos2 + 'px';
+	elementoDePlaza.style.left = elementoDePlaza.offsetLeft - pos1 + 'px';
+}
+
+function detenerArrastreElemento() {
+	document.onpointerup = null;
+	document.onpointermove = null;
+}
 }
